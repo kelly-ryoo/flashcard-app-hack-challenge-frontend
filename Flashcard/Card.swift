ca3f4b829cef5aab264d2482c3b5482d61bcef94
Vicki Yang <vzy3@cornell.edu>
2020-12-11 19:54:22 -0500

    Added network functionality and ability to view sets

---
+++ Flashcard/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Flashcard
+//
+//  Created by Vicki Yang on 12/11/20.
+//
+
+import Foundation
---
+++ Flashcard/DeckTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DeckTableViewCell.swift
+//  Flashcard
+//
+//  Created by Vicki Yang on 12/11/20.
+//
+
+import UIKit
+
+class DeckTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
